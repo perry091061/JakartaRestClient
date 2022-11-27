e638a25947157ad91b5d2d259d6fbc76dbc35bd2
PerryD <colofulmagic@localhost>
2022-11-27 16:21:50 +0000

    First commit for rest client using jakarta import rather than javax.

---
+++ restclient/src/main/java/com/example/restclient/entities/Joke.java
@@ -0,0 +1,45 @@
+package com.example.restclient.entities;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Joke {
+
+	private int id;
+	private String type;
+	private String setup;
+	private String punchline;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSetup() {
+		return setup;
+	}
+	public void setSetup(String setup) {
+		this.setup = setup;
+	}
+	public String getPunchline() {
+		return punchline;
+	}
+	public void setPunchline(String punchline) {
+		this.punchline = punchline;
+	}
+	@Override
+	public String toString() {
+		return "Joke [id=" + id + ", type=" + type + ", setup=" + setup + ", punchline=" + punchline + "]";
+	}
+	
+	
+	
+	
+}
